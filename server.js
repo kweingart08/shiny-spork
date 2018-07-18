5d0edd17c5e82439a66291fe3f9af81cc05636be
your name <you@example.com>
2018-07-18 10:25:05 -0700

    created model, initiallized mongoose

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 // dependencies
 // ------------------------------
 const express = require("express");
+const mongoose = require("mongoose")
 const app = express();
 
 // ------------------------------
@@ -29,4 +30,12 @@ app.get("/", (req, res) => {
 // ------------------------------
 app.listen(port, () => {
   console.log("I'm listening on port: ", port);
+});
+
+// ------------------------------
+// initialize mongoose
+// ------------------------------
+mongoose.connect('mongodb://localhost:27017/meancrud');
+mongoose.connection.once('open', ()=>{
+    console.log('connected to mongoose...');
 });
